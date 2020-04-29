3b1529bdb837a4eeeb94d7aeb3871f386c51b143
Barry Griffith <barrygriffith@outlook.com>
2020-04-29 15:02:49 -0400

    Built the detect language feature.  Hints need to be added.

--- translator.js
+++ translator.js
@@ -1,6 +1,9 @@
 //Click event to trigger the translation
 document.querySelector("#translate-btn").addEventListener("click", translate)
 
+
+
+
 //Function to translate text using Yandex Translate
 function translate(){
     let text = document.querySelector("#translate-input").value;
@@ -23,7 +26,28 @@ function translate(){
         document.querySelector("#output-div").innerHTML = translatedString.text[0]    
         })
     }
+    
+   
   }
+
+document.querySelector("#translate-input").addEventListener("keyup", function(){
+    detect()
+})
+
+
+let detect = () => {
+    // let test = "this is a test I hope this is enough text"
+    // let hint = ["english", "german", "spanish"]
+    let text = document.querySelector("#translate-input").value
+    fetch("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20200424T141333Z.1b8786ba0d533aeb.e4278cb06f8fa7211135a90d661bd62aa32960fd&text="+text)
+    .then(result => result.json())
+    .then(testResult => {
+       console.log(testResult.lang)
+       document.querySelector("#language-input").value = testResult.lang
+    })     
+}
+
+
 
   function hillbillian(string){
     string = string.replace(/you all/gi, "ya'll")
